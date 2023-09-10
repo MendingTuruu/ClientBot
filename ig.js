b9854c91afac347a555d102b58cf9942cbfce35c
MendingTuruu <135227605+MendingTuruu@users.noreply.github.com>
2023-09-10 23:22:11 +0700

    Create ig.js

---
+++ ig.js
@@ -0,0 +1,17 @@
+const api = require('api-dylux');
+let handler = async (m, {
+ conn,
+ text, 
+ usedPrefix, 
+ command
+ }) = > {   
+  if (!text) throw `Masukan URL Instagram!\n\n*Example:* ${ usedPrefix + command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link `
+  await m.reply('Tunggu Sebentar...')
+  api.igdl(text)
+  .then(res => conn.sendFile(m.chat, res.url_list, 'igdl.mp4', '', m))
+  .catch(console.error);
+}
+
+handler.command = handler.help = ['ig3','igdl3']
+handler.tags = ['downloader']
+module.exports = handler
