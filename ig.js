f849dfb313a5fa9b8b1c077c3b41922fa9655d30
MendingTuruu <135227605+MendingTuruu@users.noreply.github.com>
2023-09-10 23:22:41 +0700

    Update ig.js

--- ig.js
+++ ig.js
@@ -4,7 +4,7 @@ let handler = async (m, {
  text, 
  usedPrefix, 
  command
- }) = > {   
+ }) => {   
   if (!text) throw `Masukan URL Instagram!\n\n*Example:* ${ usedPrefix + command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link `
   await m.reply('Tunggu Sebentar...')
   api.igdl(text)
